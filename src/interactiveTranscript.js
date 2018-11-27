0dd7c3335d14410b15b9c2903c2a3dbf8e1787ee
zevav <zev@averba.ch>
2018-11-27 08:55:06 -0500

    new ngrok

--- src/interactiveTranscript.js
+++ src/interactiveTranscript.js
@@ -55,7 +55,7 @@ class InteractiveTranscript extends Component {
                 })
             }
         } else {
-            fetch('https://b605e748.ngrok.io/transcript?transcript_id=10')
+            fetch('https://e2a06845.ngrok.io/transcript?transcript_id=10')
                 .then(response => response.json())
                 .then(data => {
                     this.setState({ transcript: data })
