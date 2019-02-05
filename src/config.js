cb4369af9a405fd732c97a37ef4382e26b204656
zevav <zev@averba.ch>
2019-02-05 14:33:54 -0500

    ../..

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 
 export const CONFIDENCE_THRESHOLD = .87
-export const transcriptEndpoint = 'https://transcribely.ngrok.io/transcript?transcript_id=11'
-export const transcriptPostEndpoint = 'https://transcribely.ngrok.io/transcript'
+export const backEndURL = 'https://api.transcribely.co'
+export const transcriptEndpoint = `${backEndURL}/transcript?transcript_id=11`
+export const transcriptPostEndpoint = `${backEndURL}/transcript`
